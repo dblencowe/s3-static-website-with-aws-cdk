5591c06f5c532a9da8e74fb7900271d48ac20b81
Dave Blencowe <dblencowe@gmail.com>
2021-08-06 18:29:51 +0100

    Create an S3 bucket for storing our webites files & add CORS rules to the bucket

--- lib/s3-static-website-with-cdk-stack.ts
+++ lib/s3-static-website-with-cdk-stack.ts
@@ -1,9 +1,22 @@
 import * as cdk from '@aws-cdk/core';
+import { RemovalPolicy } from '@aws-cdk/core';
+import { Bucket, HttpMethods } from "@aws-cdk/aws-s3";
 
 export class S3StaticWebsiteWithCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const domainName = 'example.com'
+
+    const bucket = new Bucket(this, 'SiteFiles', {
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html'
+    })
+
+    bucket.addCorsRule({
+      allowedMethods: [HttpMethods.DELETE, HttpMethods.GET, HttpMethods.HEAD, HttpMethods.POST, HttpMethods.PUT],
+      allowedOrigins: [domainName]
+    })
   }
 }
