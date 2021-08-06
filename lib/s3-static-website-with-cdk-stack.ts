effd8019e81a73b4edb08ebc3fddcc567adbe320
Dave Blencowe <dblencowe@gmail.com>
2021-08-06 19:07:16 +0100

    Adds a Route53 zone and a wildcard certificate, then attaches the certificate to the cloudfront distribution

--- lib/s3-static-website-with-cdk-stack.ts
+++ lib/s3-static-website-with-cdk-stack.ts
@@ -3,12 +3,22 @@ import { RemovalPolicy } from '@aws-cdk/core';
 import { Bucket, HttpMethods } from "@aws-cdk/aws-s3";
 import { CloudFrontWebDistribution, OriginAccessIdentity } from "@aws-cdk/aws-cloudfront";
 import { BucketDeployment, Source } from "@aws-cdk/aws-s3-deployment";
+import { PublicHostedZone } from "@aws-cdk/aws-route53";
+import { Certificate, CertificateValidation } from "@aws-cdk/aws-certificatemanager";
 
 export class S3StaticWebsiteWithCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const domainName = 'example.com'
+    const domain = new PublicHostedZone(this, 'Domain', {
+      zoneName: domainName
+    })
+
+    const certificate = new Certificate(this, 'SSL Cert', {
+      domainName,
+      validation: CertificateValidation.fromDns(domain)
+    })
 
     const bucket = new Bucket(this, 'SiteFiles', {
       publicReadAccess: true,
@@ -41,7 +51,14 @@ export class S3StaticWebsiteWithCdkStack extends cdk.Stack {
 
     const distribution = new CloudFrontWebDistribution(this, 'CDN', {
       errorConfigurations,
-      originConfigs
+      originConfigs,
+      viewerCertificate: {
+        aliases: [domainName],
+        props: {
+          acmCertificateArn: certificate.certificateArn,
+          sslSupportMethod: "sni-only",
+        },
+      },
     })
 
     new cdk.CfnOutput(this, 'Web Address', {
