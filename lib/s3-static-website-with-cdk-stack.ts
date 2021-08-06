bbffa74c918162a6b8a93ceed835d6111549c029
Dave Blencowe <dblencowe@gmail.com>
2021-08-06 18:44:44 +0100

    Creates a cloudfront distribution to front the S3 bucket

--- lib/s3-static-website-with-cdk-stack.ts
+++ lib/s3-static-website-with-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import { RemovalPolicy } from '@aws-cdk/core';
 import { Bucket, HttpMethods } from "@aws-cdk/aws-s3";
+import { CloudFrontWebDistribution, OriginAccessIdentity } from "@aws-cdk/aws-cloudfront";
 
 export class S3StaticWebsiteWithCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -17,6 +18,33 @@ export class S3StaticWebsiteWithCdkStack extends cdk.Stack {
     bucket.addCorsRule({
       allowedMethods: [HttpMethods.DELETE, HttpMethods.GET, HttpMethods.HEAD, HttpMethods.POST, HttpMethods.PUT],
       allowedOrigins: [domainName]
+    })
+
+    const cfOAI = new OriginAccessIdentity(this, 'AccessId', {
+      comment: 'website origin access ID'
+    })
+
+    const errorConfigurations = [{
+      errorCode: 404,
+      responseCode: 200,
+      responsePagePath: '/index.html'
+    }]
+
+    const originConfigs = [{
+      s3OriginSource: {
+        s3BucketSource: bucket,
+        originAccessIdentity: cfOAI
+      },
+      behaviors: [{ isDefaultBehavior: true }]
+    }]
+
+    const distribution = new CloudFrontWebDistribution(this, 'CDN', {
+      errorConfigurations,
+      originConfigs
+    })
+
+    new cdk.CfnOutput(this, 'Web Address', {
+      value: distribution.distributionDomainName
     })
   }
 }
